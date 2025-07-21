14b9f9225f51dd84ee1deb146b1a9a2290a728ba
unknown <mahmudurrahmance75@gmail.com>
2025-07-21 22:10:18 +0600

    code 21.07.2025

--- src/task/AddTaskModel.jsx
+++ src/task/AddTaskModel.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 
-export default function AddTaskModel({onSave}) {
+export default function AddTaskModel({onSave, taskToUpdate}) {
 
-    const [task, setTask] = useState({
+    const [task, setTask] = useState(taskToUpdate || {
+        'id': crypto.randomUUID(),
         'title': "",
         'description': "",
         'tags': [],
         'priority': "",
         'isFavorite': false,
     })
+
+
+    const [isAdd, setIsAdd] = useState(Object.is(taskToUpdate, null));
 
     const handleChange = (evt) => {
         const name = evt.target.name;
@@ -30,7 +34,7 @@ export default function AddTaskModel({onSave}) {
       <div className="bg-[rgba(0,0,0,0.7)] h-full w-full absolute z-10 top-0 left-0"></div>
       <form className="mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11 z-20 absolute top-1/4 left-1/4">
         <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
-          Add New Task
+          { isAdd ? "Add New Task" : "Edit Task" }
         </h2>
 
         <div className="space-y-9 text-white lg:space-y-10">
@@ -97,9 +101,9 @@ export default function AddTaskModel({onSave}) {
           <button
             type="submit"
             className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
-            onClick={() => onSave(task)}
+            onClick={() => onSave(task, isAdd)}
           >
-            Create new Task
+            Save
           </button>
         </div>
       </form>
--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -4,45 +4,64 @@ import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 import AddTaskModel from "./AddTaskModel";
 
-export default function TaskBoard(){
+export default function TaskBoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React Native",
+    description: "I want to learn react",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavorite: true,
+  };
 
-    const defaultTask = {
-        'id': crypto.randomUUID(),
-        'title': "Learn React Native",
-        'description': "I want to learn react",
-        'tags': ["web", "react", "js"],
-        'priority': "High",
-        'isFavorite': true,
+  const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModel, setShowAddModel] = useState(false);
+  const [taskToUpdate, setTaskToUpdate] = useState(null);
+
+  // function handleAddTask(task){
+  //     console.log("adding a task", task);
+  // }
+
+  function handleAddTask(newTask, isAdd) {
+    if(isAdd){
+        setTasks([...tasks, newTask]);
+    }else{
+        setTasks(
+            tasks.map((task) => {
+                if(task.id === newTask.id){
+                    return newTask;
+                }
+                return task;
+            })
+        )
     }
+    
+    setShowAddModel(false); // close modal after adding
+  }
 
-    const [tasks, setTasks] = useState([defaultTask]);
 
-    const [showAddModel, setShowAddModel] = useState(false);
+  function handleEditTask(task){
+    setTaskToUpdate(task);
+    setShowAddModel(true);
+  }
 
-    function handleAddTask(task){
-        console.log("adding a task", task);
-    }
 
-    return(
-        <section className="mb-20" id="tasks">
 
-            {showAddModel && <AddTaskModel onSave = {handleAddTask} />}
-		
-		<div className="container">
-			
-		<div className="p-2 flex justify-end">
-			<SearchTask/>
-		</div>
-		
-			<div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-				
-                <TaskAction onAddClick={() => setShowAddModel(true)}/>
+  return (
+    <section className="mb-20" id="tasks">
+      {showAddModel && <AddTaskModel onSave={handleAddTask} taskToUpdate={taskToUpdate} />}
 
-				<TaskList tasks = {tasks} />
+      <div className="container">
+        <div className="p-2 flex justify-end">
+          <SearchTask />
+        </div>
 
-			</div>
-		</div>
-	</section>
+        <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+          <TaskAction onAddClick={() => setShowAddModel(true)} />
 
-    );
+          <TaskList tasks={tasks} onEdit = {handleEditTask}/>
+        </div>
+      </div>
+    </section>
+  );
 }
