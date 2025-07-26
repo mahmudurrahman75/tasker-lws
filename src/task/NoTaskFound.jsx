9992201548cebc0a2757c8b75b474f1fda3550f9
unknown <mahmudurrahmance75@gmail.com>
2025-07-26 20:46:40 +0600

    last code

---
+++ src/task/NoTaskFound.jsx
@@ -0,0 +1,5 @@
+export default function NoTaskFounds(){
+    return (
+        <p className="text-center text-3xl">No Task Found. Please add one.</p>
+    )
+}
--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -3,6 +3,7 @@ import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 import AddTaskModel from "./AddTaskModel";
+import NoTaskFound from "./NoTaskFound";
 
 export default function TaskBoard() {
   const defaultTask = {
@@ -22,7 +23,7 @@ export default function TaskBoard() {
   //     console.log("adding a task", task);
   // }
 
-  function handleAddTask(newTask, isAdd) {
+  function handleAddEditTask(newTask, isAdd) {
     if(isAdd){
         setTasks([...tasks, newTask]);
     }else{
@@ -46,20 +47,57 @@ export default function TaskBoard() {
   }
 
 
+  function handleCloseClick(){
+    setShowAddModel(false);
+    setTaskToUpdate(null);
+  }
+
+  function handleDeleteTask(taskId){
+    const taskAfterDelete = tasks.filter(task => task.id !== taskId);
+    setTasks(taskAfterDelete);
+  }
+
+  function handleDeleteAllClick(){
+    tasks.length = 0;
+    setTasks([...tasks]);
+  }
+
+  function handleFavorite(taskId){
+    const taskIndex = tasks.findIndex(task => task.id === taskId); 
+    const newTasks = [...tasks];
+    newTasks[taskIndex].isFavorite = !newTasks[taskIndex].isFavorite;
+    setTasks(newTasks);
+  }
+
+  function handleSearch(searchTerm){
+    //console.log(searchTerm);
+
+    const filtered = tasks.filter((task) => task.title.toLowerCase().includes(searchTerm.toLowerCase()));
+    setTasks([...filtered]);
+  }
+
+
 
   return (
     <section className="mb-20" id="tasks">
-      {showAddModel && <AddTaskModel onSave={handleAddTask} taskToUpdate={taskToUpdate} />}
+      {showAddModel && <AddTaskModel onSave={handleAddEditTask} onCloseClick={handleCloseClick} taskToUpdate={taskToUpdate} />}
 
       <div className="container">
         <div className="p-2 flex justify-end">
-          <SearchTask />
+
+          <SearchTask onSearch={handleSearch} />
+
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskAction onAddClick={() => setShowAddModel(true)} />
+          <TaskAction onAddClick={() => setShowAddModel(true)} onDeleteAllClick = {handleDeleteAllClick} />
 
-          <TaskList tasks={tasks} onEdit = {handleEditTask}/>
+          {
+            tasks.length > 0 ?
+          <TaskList tasks={tasks} onEdit = {handleEditTask} onDelete = {handleDeleteTask} onFav = {handleFavorite}/>
+          : (<NoTaskFound/>) 
+          }
+
         </div>
       </div>
     </section>
